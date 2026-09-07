b77b754e09717a6786d36fafe01b5cdbf25c6f47
zahil <zahil@zahils-MacBook-Air.local>
2026-09-07 14:52:20 +0800

    railway fix

--- b2b/login.js
+++ b2b/login.js
@@ -1,6 +1,6 @@
 // login.js (B2B login page)
 
-const API_URL = 'http://booking-system-production-fa13.up.railway.app';
+const API_URL = 'https://booking-system-production-fa13.up.railway.app';
 
 document.getElementById('loginBtn').addEventListener('click', doLogin);
 
